97968c9ffc94c5189031b2b296304401836f24e3
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2023-03-13 16:01:49 -0700

    finalized the development of basic loading animation component. Might change later.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import './assets/App.css';
-import anime from 'animejs';
+import LoadingAnimation from './components/loading-component/loading-component';
 
 function App() {
-  anime({
-    targets: '.fading-circle',
-    easing: 'easeOutExpo',
-    loop: true,
-    scale: [2, 10],
-    opacity: {
-      value: [1, 0],
-      easing: 'linear',
-      duration: 1500,
-    },
-    delay: (el, i) => 200 * i,
-  })
-
   return (
     <div className="App">
       <header className="App-header">
-        <div className="spinner-wrapper">
-          <div className="fading-circle"></div>
-          <div className="fading-circle"></div>
-          <div className="fading-circle"></div>
-        </div>
+        <LoadingAnimation />
       </header>
     </div>
   );
--- src/components/loading-component/loading-component.js
+++ src/components/loading-component/loading-component.js
@@ -1,12 +1,33 @@
-let rowsY = 10;
-let columnsX = 25;
+import React, { Component } from 'react';
+import anime from 'animejs';
 
-let container = document.querySelector('.container-animation');
-
-for (let i = 0; i < rowsY * columnsX; i++) {
-    let box = document.createElement('div');
-    box.setAttribute('class', 'box');
-    container.appendChild(box);
+class LoadingAnimation extends Component {
+    componentDidMount() {
+        anime({
+            targets: '.fading-circle',
+            easing: 'easeOutExpo',
+            loop: true,
+            scale: [0, 4],
+            opacity: {
+                value: [1, 0],
+                easing: 'linear',
+                duration: 800,
+            },
+            delay: (el, i) => 200 * i,
+        });
+    }
+    render() {
+        return(
+            <div className="container">
+                <h1 className="display-1">Aesculapius</h1>
+                <div className="spinner-wrapper row justify-content-center align-items-center">
+                    <div className="fading-circle"></div>
+                    <div className="fading-circle"></div>
+                    <div className="fading-circle"></div>
+                </div>
+            </div>
+        );
+    }
 }
 
-console.log('hello world')
+export default LoadingAnimation;
