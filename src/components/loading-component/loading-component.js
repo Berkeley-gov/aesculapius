bb516a33aa97afd875491b1e078abbcc0f386502
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2023-03-21 19:06:11 -0700

    finished the react loading component and page UI

--- src/components/loading-component/loading-component.js
+++ src/components/loading-component/loading-component.js
@@ -4,28 +4,53 @@ import anime from 'animejs';
 class LoadingAnimation extends Component {
     componentDidMount() {
         anime({
-            targets: '.fading-circle',
-            easing: 'easeOutExpo',
-            loop: true,
-            scale: [0, 4],
-            opacity: {
-                value: [1, 0],
-                easing: 'linear',
-                duration: 800,
-            },
-            delay: (el, i) => 200 * i,
+            targets: '.loading-bar',
+            translateX: '50px',
+            easing: 'easeInOutQuad',
+            direction: 'alternate',
+            loop: true
+        });
+
+        anime.timeline({loop: true})
+        .add({
+            targets: '.text-container .letter',
+            opacity: [0,1],
+            easing: "easeInOutQuad",
+            duration: 2250,
+            delay: function(el, i) {
+            return 150 * (i+1)
+            }
+        }).add({
+            targets: '.text-container .letter',
+            opacity: [1,0],
+            easing: "easeInOutQuad",
+            duration: 2250,
+            delay: function(el, i) {
+            return 150 * (i+1)
+            }
         });
     }
     render() {
         return(
-            <div className="container">
-                <h1 className="display-1">Aesculapius</h1>
-                <div className="spinner-wrapper row justify-content-center align-items-center">
-                    <div className="fading-circle"></div>
-                    <div className="fading-circle"></div>
-                    <div className="fading-circle"></div>
+            <main>
+                <div className="text-container">
+                    <span className="letter">A</span>
+                    <span className="letter">e</span>
+                    <span className="letter">s</span>
+                    <span className="letter">c</span>
+                    <span className="letter">u</span>
+                    <span className="letter">l</span>
+                    <span className="letter">a</span>
+                    <span className="letter">p</span>
+                    <span className="letter">i</span>
+                    <span className="letter">u</span>
+                    <span className="letter">s</span>
                 </div>
-            </div>
+
+                <div className="loading-container">
+                    <div className="loading-bar shadow"></div>
+                </div>
+            </main>    
         );
     }
 }
